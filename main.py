1c9bfe82ffdad49aca9d5e2d8a7071b27bc0255c
SmartPolarBear <CleverCoolBear@outlook.com>
2022-03-09 11:36:52 +0800

    implement k-means

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import argparse
+from typing import Final
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class KMeans:
+    def __init__(self, feats: pd.DataFrame, k: int):
+        self.tries = 0
+        self.feats = feats
+        self.k = k
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def _wcss(self, centroids, cluster) -> float:
+        ret = 0.0
+        for i, val in enumerate(self.feats.values):
+            ret += np.sqrt(
+                (centroids[int(cluster[i]), 0] - val[0]) ** 2 + (centroids[int(cluster[i]), 1] - val[1]) ** 2)
+        return ret
+
+    def cluster(self, max_tries: int = 32767):
+        cluster = np.zeros(self.feats.shape[0])
+        centroids = self.feats.sample(n=self.k).values
+
+        self.tries = 0
+        while self.tries < max_tries:
+            self.tries += 1
+
+            for id, row in enumerate(self.feats.values):
+                min_dist = float('inf')
+                for cid, centroid in enumerate(centroids):
+                    dist = np.sqrt((centroid[0] - row[0]) ** 2 + (centroid[1] - row[1]) ** 2)
+                    if dist < min_dist:
+                        min_dist, cluster[id] = dist, cid
+            clustered_centroids = self.feats.copy().groupby(by=cluster).mean().values
+            if np.count_nonzero(centroids - clustered_centroids) == 0:
+                break
+            else:
+                centroids = clustered_centroids
+        return centroids, cluster, self._wcss(centroids, cluster)
+
+
+def clustering(path: str):
+    data = pd.read_csv(path)
+    data.insert(1, "SepalSquare", data["SepalLengthCm"] * data["SepalWidthCm"])
+    data.insert(1, "PetalSquare", data["PetalLengthCm"] * data["PetalWidthCm"])
+    print(data.describe(), data)
+
+    # sb.pairplot(data, vars=data.columns[1:7], hue="Species")
+    # plt.show()
+
+    feats = data.iloc[:, [1, 2]]
+    print(feats)
+
+    km: KMeans = KMeans(feats=feats, k=3)
+    print(km.cluster())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="KMeans Clustering")
+    parser.add_argument("--dataset", help="The path to the dataset file in CSV format")
+
+    args = parser.parse_args()
+
+    dataset: Final = str(args.dataset)
+
+    print("Dataset: {}".format(dataset))
+
+    clustering(dataset)
